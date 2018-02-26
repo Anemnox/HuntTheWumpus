52b2983dabbb6c2b557d452068edfec92a7b9a53
Jang <SN317602@kent.k12.wa.us>
2018-02-26 15:26:54 -0800

    WumpusWindow
    - Graphic User interface initial commit

---
+++ src/main/WumpusWindow.java
@@ -0,0 +1,32 @@
+package main;
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+public class WumpusWindow extends JFrame{
+	private int height;
+	private int width;
+	
+	public WumpusWindow(int width, int height) {
+		this.height = height;
+		this.width = width;
+		setSize(this.width, this.height);
+		setVisible(true);
+		setBackground(Color.LIGHT_GRAY);
+		setResizable(false);
+		setUndecorated(true);
+	}
+	
+	
+	
+	@Override
+	public int getWidth() {
+		return width;
+	}
+	
+	@Override
+	public int getHeight() {
+		return height;
+	}
+}
