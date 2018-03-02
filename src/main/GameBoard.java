494fee56949be2032818d511cb8480caf67a0a98
Peterson <SN377907@kent.k12.wa.us>
2018-03-02 14:26:20 -0800

    Beginning of work on the game map

--- src/main/GameBoard.java
+++ src/main/GameBoard.java
@@ -1,8 +1,22 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // This class stores the data for the map, where everything is located, and the player's data
+
+enum RoomFeature { PLAYER, WUMPUS, BAT, PIT }
+
 public class GameBoard 
 {
+	// In my idea of how this will work, there will be a 2D List of rooms. The first index is room number.
+	// The second index accesses a List of enumerations that show what all is in the room
+	// vv Room #   vv ArrayList of things in room
+	List<List<RoomFeature>> rooms = new ArrayList<List<RoomFeature>>();
+	// Why I can make an ArrayList go into a List and not a List into a List is beyond me
 	
-
+	
+	
 }
+
+
