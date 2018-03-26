14d6cc1ee971a3be9f44b14f7bc9a02423ff453e
Peterson <SN377907@kent.k12.wa.us>
2018-03-26 15:15:59 -0700

    Progress on GameBoard.java
    Look at me ma, I'm being productive

--- src/main/GameBoard.java
+++ src/main/GameBoard.java
@@ -1,93 +1,88 @@
-	package main;
+package main;
 
 public class GameBoard  implements RunOnGameLoop
 {
 	// The GameBoard handles the location of everything on the map
+	private int xCoor;
+	private int yCoor;
 	
+	public GameBoard(int x, int y) {
+		xCoor = x;
+		yCoor = y;
+	}
 	
+	// Get and Set commands for the x coordinate
+	public int getX() {
+		return xCoor;
+	}
 	
-	/*
-	private class Hex3 {
-		int x;
-		int y;
-		int z;
-		
-		public Hex3(int inX, int inY, int inZ) {
-			x = inX;
-			y = inY;
-			z = inZ;
-			
-			if(x + y + z != 0) {
-				System.out.println("ERROR! [IMP_FORM] 3D coordinates are not in a proper format!");
-			}
-		}
-		
-		public int get(char desiredVector) {
-			// This function returns the x, y, or z of the Hex3 coordinates. Returns -10000 if x, y, or z isn't returned
-			switch (desiredVector) {
-				case 'x':
-					return x;
-				case 'y':
-					return y;
-				case 'z':
-					return z;
-				default:
-					System.out.println("ERROR! [INC_INDEX] Requested coordinate is not x, y, or z!");
-					return -10000;
-			}
-		}
-		
-		public boolean matches(Hex3 compareTarget) {
-			if(x == compareTarget.get('x') && y == compareTarget.get('y') && z == compareTarget.get('z')) {
-				return true;
-			}
-			else {
-				return false;
-			}
-		}
-		
-		// TODO Make sure the move command loops the player around the map
-		public void move(int direction) {
-			// Moves this coordinate in one of six directions.
-			// Input:	0	| 1		  | 2		  | 3		| 4		   | 5
-			// Moves:	Up	| UpRight | DownRight | Down	| DownLeft | UpLeft
-			// Prints an error in the console if an int outside of 0-5 is used.
-			switch(direction) {
-				case 0:
-					y += 1;
-					z -= 1;
-					break;
-				case 1:
-					x += 1;
-					z -= 1;
-					break;
-				case 2:
-					x += 1;
-					y -= 1;
-					break;
-				case 3:
-					z += 1;
-					y -= 1;
-					break;
-				case 4:
-					z += 1;
-					x -= 1;
-					break;
-				case 5:
-					y += 1;
-					x -= 1;
-					break;
-				default:
-					System.out.println("ERROR! [OOB] Out of Bounds on Hex3's move function!");
-			}
-		}
-		*/
-		
-		
+	public void setX(int input) {
+		xCoor = input;
+	}
 	
-
-
-
+	// Get and Set commands for the y coordinate
+	public int getY() {
+		return yCoor;
+	}
+	
+	public void setY(int input) {
+		yCoor = input;
+	}
+	
+	// The TestMove command checks if a movement can be made, using the Cave's walls
+	public void testMove(int moveIndex) {
+		switch (moveIndex) {
+			case 0:
+				// TODO Get Everett's code 
+				break;
+			case 1:
+				
+				break;
+			case 2:
+				
+				break;
+			case 3:
+				
+				break;
+			case 4:
+				
+				break;
+			case 5:
+				
+				break;
+			default:
+				System.out.println("Error. Incorrect index");
+		}
+	}
+	
+	// The Move command is used to move the object's coordinates. 
+	public void move(int moveIndex) {
+		switch (moveIndex) {
+			case 0:
+				yCoor--;
+				break;
+			case 1:
+				xCoor++;
+				yCoor--;
+				break;
+			case 2:
+				xCoor++;
+				break;
+			case 3:
+				yCoor++;
+				break;
+			case 4:
+				xCoor--;
+				break;
+			case 5:
+				xCoor--;
+				yCoor--;
+				break;
+			default:
+				System.out.println("Error. Incorrect index");
+		}
+	}
+	
 
 	@Override
 	public void update(double tick) {
